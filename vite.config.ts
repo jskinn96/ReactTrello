7c7c831fc30a8eaabb23c45e232c04d4140cddcb
jisu kim <rla2877622@naver.com>
2025-04-04 17:32:53 +0900

    컨피그 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/jskinn96/ReactTrello",
+  base: "/ReactTrello",
   plugins: [react()],
 })
